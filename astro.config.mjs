bdbd2dc2cff7d8db1cd8fe8c9ce49230d19c691c
nyphalim <nyphalim@gmail.com>
2025-03-26 19:50:42 +0500

    Документация для класса \IH\CheckingPartnerProgram

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import starlightThemeRapide from 'starlight-theme-rapide'
 
 // https://astro.build/config
 export default defineConfig({
 	integrations: [
 		starlight({
+			plugins: [starlightThemeRapide()],
 			title: 'IH.Docs',
 			sidebar: [
 				{
